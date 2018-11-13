a573c32a6fc1f25c4f0be34d271e14380bd0c149
michelle <yunxu_master@126.com>
2018-11-13 17:55:39 +0800

    modify TokenTicker

--- src/main/scala/org/loopring/marketcap/crawler/TokenTickerCrawlerActor.scala
+++ src/main/scala/org/loopring/marketcap/crawler/TokenTickerCrawlerActor.scala
@@ -17,7 +17,6 @@
 package org.loopring.marketcap.crawler
 
 import java.text.SimpleDateFormat
-
 import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Timers }
 import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.model.{ HttpMethods, HttpRequest, HttpResponse, StatusCodes }
@@ -26,9 +25,8 @@ import akka.util.Timeout
 import org.loopring.marketcap.SeqTpro
 import org.loopring.marketcap.broker.HttpConnector
 import org.loopring.marketcap.proto.data._
-import scalapb.json4s.Parser
-
-import scala.concurrent.Future
+import scalapb.json4s.{ JsonFormat, Parser }
+import scala.concurrent.{ Await, Future }
 import scala.concurrent.duration._
 
 class TokenTickerCrawlerActor(tokenTickerServiceActor: ActorRef)(
@@ -40,59 +38,89 @@ class TokenTickerCrawlerActor(tokenTickerServiceActor: ActorRef)(
   implicit val ec = context.system.dispatcher
 
   val config = system.settings.config
-
   val connection = https(config.getString("cmc-config.prefix_url"))
   val limitSize = config.getString("cmc-config.limitSize").toInt
-  val pageCount = config.getString("cmc-config.pageCount").toInt
   val appKey = config.getString("cmc-config.api_key")
 
-  val rawHeader = RawHeader(config.getString("cmc-config.header"), appKey)
-  val convertCurrency = Seq("CNY", "USD", "ETH", "LRC", "USDT", "TUSD")
+  //val convertCurrency = config.getString("cmc-config.convertCurrency")
 
+  val rawHeader = RawHeader(config.getString("cmc-config.header"), appKey)
   val parser = new Parser(preservingProtoFieldNames = true) //protobuf 序列化为json不使用驼峰命名
 
   override def preStart(): Unit = {
     //daliy schedule token's ticker info
-    timers.startPeriodicTimer("cronSyncTokenTicker", "syncTokenTicker", 600 seconds)
+    timers.startSingleTimer("cronSyncTokenTicker", "syncTokenTicker", 30 seconds)
   }
 
   override def receive: Receive = {
     case _: String ⇒
-      convertCurrency.foreach {
-        currency =>
-          getReq(currency)
-          Thread.sleep(50)
+      val futureResult = for {
+        usdTickers ← getTokenTickers("USD")
+        marketTickers = getMarketUSDQuote(usdTickers)
+      } yield {
+        for {
+          usdTicker ← usdTickers
+          usdQuote = usdTicker.quote.get("USD") //先找到每个token的usd quote
+          _ = require(usdQuote.isDefined, s"can not found ${usdTicker.symbol} quote for USD")
+          priceQuote = usdQuote.get
+        } yield {
+          val quoteMap = marketTickers.foldLeft(usdTicker.quote) { (map, marketQuote) ⇒
+            //添加市场代币的Quote ("LRC", "ETH", "TUSD", "USDT")
+            map + (marketQuote._1 → convertQuote(priceQuote, marketQuote._2))
+          }
+          //更新token的quote属性
+          usdTicker.copy(quote = quoteMap)
+        }
       }
+
+      val result = Await.result(futureResult, 10 seconds)
+      val fixGroup = result.grouped(20)
+      fixGroup.foreach {
+        group =>
+          tokenTickerServiceActor ! convertTO(group)
+      }
+
   }
 
-  def getReq(currency: String): Unit = {
-    //max support limitSize=5000, now set pageCount = 1
-    (0 until pageCount).map { i =>
+  //获取cmc's token对锚定币convertCurrency的价格
+  def getTokenTickers(convertCurrency: String): Future[Seq[CMCTickerData]] = {
+    val uri = s"/v1/cryptocurrency/listings/latest?start=1&limit=${limitSize}&convert=${convertCurrency}"
+    get(HttpRequest(uri = uri, method = HttpMethods.GET).withHeaders(rawHeader)) {
+      case HttpResponse(StatusCodes.OK, _, entity, _) =>
+        entity.dataBytes.map(_.utf8String).runReduce(_ + _).map(parser.fromJsonString[TickerDataInfo]).map(_.data)
 
-      val uri = s"/v1/cryptocurrency/listings/latest?start=${i * limitSize + 1}&limit=${limitSize}&convert=${currency}"
-
-      get(HttpRequest(uri = uri, method = HttpMethods.GET).withHeaders(rawHeader)) {
-        case HttpResponse(StatusCodes.OK, _, entity, _) =>
-          entity.dataBytes.map(_.utf8String).runReduce(_ + _).map { dataInfoStr ⇒
-            val dataInfo = parser.fromJsonString[TickerDataInfo](dataInfoStr)
-            val fixGroup = dataInfo.data.grouped(100)
-            fixGroup.foreach {
-              group =>
-                tokenTickerServiceActor ! convertTO(group, currency)
-            }
-          }
-
-        case _ =>
-          log.error("get ticker data from coinmarketcap failed")
-          Future.successful(Unit)
-      }
+      case _ =>
+        log.error("get ticker data from coinmarketcap failed")
+        Future.successful(Seq[CMCTickerData]())
     }
   }
 
-  def convertTO(tickers: Seq[CMCTickerData], currency: String): SeqTpro[TokenTickerInfo] = {
+  //找到市场代币对USD的priceQuote
+  def getMarketUSDQuote(tickers: Seq[CMCTickerData]): Seq[(String, Quote)] = {
+    markets.map { s ⇒
+      val priceQuote = tickers.find(_.symbol == s).flatMap(_.quote.get("USD"))
+      require(priceQuote.isDefined, s"can not found ${s} / USD")
+      (s, priceQuote.get)
+    }
+  }
 
+  // 初始化交易市场
+  lazy val markets = Seq("LRC", "ETH", "TUSD", "USDT")
+
+  //锚定市场币的priceQuote换算
+  def convertQuote(tokenQuote: Quote, marketQuote: Quote): Quote = {
+    val price = toDouble(BigDecimal(tokenQuote.price / marketQuote.price))
+    val volume_24h = toDouble(BigDecimal(tokenQuote.volume24H / tokenQuote.price) * price)
+    val market_cap = toDouble(BigDecimal(tokenQuote.marketCap / tokenQuote.price) * price)
+    val percent_change_1h = BigDecimal(1 + tokenQuote.percentChange1H) / BigDecimal(1 + marketQuote.percentChange1H) - 1
+    val percent_change_24h = BigDecimal(1 + tokenQuote.percentChange24H) / BigDecimal(1 + marketQuote.percentChange24H) - 1
+    val percent_change_7d = BigDecimal(1 + tokenQuote.percentChange7D) / BigDecimal(1 + marketQuote.percentChange7D) - 1
+    val last_updated = marketQuote.lastUpdated
+    Quote(price, volume_24h, toDouble(percent_change_1h), toDouble(percent_change_24h), toDouble(percent_change_7d), toDouble(market_cap), last_updated)
+  }
+
+  def convertTO(tickers: Seq[CMCTickerData]): SeqTpro[TokenTickerInfo] = {
     val f = tickers.foldLeft(Seq.empty[TokenTickerInfo]) { (tickerinfos, ticker) ⇒
-
       val id = ticker.id
       val name = ticker.name
       val symbol = ticker.symbol
@@ -101,23 +129,30 @@ class TokenTickerCrawlerActor(tokenTickerServiceActor: ActorRef)(
       val circulatingSupply = ticker.circulatingSupply
       val totalSupply = ticker.totalSupply
       val maxSupply = ticker.maxSupply
-      val market = currency
-      val quote = ticker.quote.apply(currency)
-      val price = quote.price
-      val volume24h = quote.volume24H
-      val marketCap = quote.marketCap
-      val percentChange1h = quote.percentChange1H
-      val percentChange24h = quote.percentChange24H
-      val percentChange7d = quote.percentChange7D
-      val utcFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS Z")
-      val lastUpdated = utcFormat.parse(quote.lastUpdated.replace("Z", " UTC")).getTime / 1000
-      val tickerInfo = TokenTickerInfo(id, name, symbol, websiteSlug, market, rank, circulatingSupply, totalSupply, maxSupply,
-        price, volume24h, marketCap, percentChange1h, percentChange24h, percentChange7d, lastUpdated)
 
-      tickerinfos :+ tickerInfo
+      ticker.quote.foreach {
+        quoteTicker =>
+          val market = quoteTicker._1
+          val quote = quoteTicker._2
+          val price = quote.price
+          val volume24h = quote.volume24H
+          val marketCap = quote.marketCap
+          val percentChange1h = quote.percentChange1H
+          val percentChange24h = quote.percentChange24H
+          val percentChange7d = quote.percentChange7D
+          val utcFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS Z")
+          val lastUpdated = utcFormat.parse(quote.lastUpdated.replace("Z", " UTC")).getTime / 1000
+          val tickerInfo = TokenTickerInfo(id, name, symbol, websiteSlug, market, rank, circulatingSupply, totalSupply, maxSupply,
+            price, volume24h, marketCap, percentChange1h, percentChange24h, percentChange7d, lastUpdated)
+           tickerinfos :+ tickerInfo
+      }
+      tickerinfos
     }
-
     SeqTpro(f)
+  }
+
+  def toDouble: PartialFunction[BigDecimal, Double] = {
+    case s: BigDecimal ⇒ scala.util.Try(s.setScale(8, BigDecimal.RoundingMode.HALF_UP).toDouble).toOption.getOrElse(0)
   }
 
 }
---
+++ src/main/scala/org/loopring/marketcap/crawler/TokenTickerCrawlerActorBackup.scala
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2018 Loopring Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.loopring.marketcap.crawler
+
+import java.text.SimpleDateFormat
+
+import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Timers }
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.model.{ HttpMethods, HttpRequest, HttpResponse, StatusCodes }
+import akka.stream.ActorMaterializer
+import akka.util.Timeout
+import org.loopring.marketcap.SeqTpro
+import org.loopring.marketcap.broker.HttpConnector
+import org.loopring.marketcap.proto.data._
+import scalapb.json4s.Parser
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+class TokenTickerCrawlerActorBackup(tokenTickerServiceActor: ActorRef)(
+  implicit
+  val system: ActorSystem,
+  val mat: ActorMaterializer) extends Actor with HttpConnector with Timers with ActorLogging {
+
+  implicit val timeout = Timeout(5 seconds)
+  implicit val ec = context.system.dispatcher
+
+  val config = system.settings.config
+
+  val connection = https(config.getString("cmc-config.prefix_url"))
+  val limitSize = config.getString("cmc-config.limitSize").toInt
+  val pageCount = config.getString("cmc-config.pageCount").toInt
+  val appKey = config.getString("cmc-config.api_key")
+
+  val rawHeader = RawHeader(config.getString("cmc-config.header"), appKey)
+  val convertCurrency = Seq("CNY", "USD", "ETH", "LRC", "USDT", "TUSD")
+
+  val parser = new Parser(preservingProtoFieldNames = true) //protobuf 序列化为json不使用驼峰命名
+
+  override def preStart(): Unit = {
+    //daliy schedule token's ticker info
+    timers.startPeriodicTimer("cronSyncTokenTicker", "syncTokenTicker", 600 seconds)
+  }
+
+  override def receive: Receive = {
+    case _: String ⇒
+      convertCurrency.foreach {
+        currency =>
+          getReq(currency)
+          Thread.sleep(50)
+      }
+  }
+
+  def getReq(currency: String): Unit = {
+    //max support limitSize=5000, now set pageCount = 1
+    (0 until pageCount).map { i =>
+
+      val uri = s"/v1/cryptocurrency/listings/latest?start=${i * limitSize + 1}&limit=${limitSize}&convert=${currency}"
+
+      get(HttpRequest(uri = uri, method = HttpMethods.GET).withHeaders(rawHeader)) {
+        case HttpResponse(StatusCodes.OK, _, entity, _) =>
+          entity.dataBytes.map(_.utf8String).runReduce(_ + _).map { dataInfoStr ⇒
+            val dataInfo = parser.fromJsonString[TickerDataInfo](dataInfoStr)
+            val fixGroup = dataInfo.data.grouped(100)
+            fixGroup.foreach {
+              group =>
+                tokenTickerServiceActor ! convertTO(group, currency)
+            }
+          }
+
+        case _ =>
+          log.error("get ticker data from coinmarketcap failed")
+          Future.successful(Unit)
+      }
+    }
+  }
+
+  def convertTO(tickers: Seq[CMCTickerData], currency: String): SeqTpro[TokenTickerInfo] = {
+
+    val f = tickers.foldLeft(Seq.empty[TokenTickerInfo]) { (tickerinfos, ticker) ⇒
+
+      val id = ticker.id
+      val name = ticker.name
+      val symbol = ticker.symbol
+      val websiteSlug = ticker.slug
+      val rank = ticker.cmcRank
+      val circulatingSupply = ticker.circulatingSupply
+      val totalSupply = ticker.totalSupply
+      val maxSupply = ticker.maxSupply
+      val market = currency
+      val quote = ticker.quote.apply(currency)
+      val price = quote.price
+      val volume24h = quote.volume24H
+      val marketCap = quote.marketCap
+      val percentChange1h = quote.percentChange1H
+      val percentChange24h = quote.percentChange24H
+      val percentChange7d = quote.percentChange7D
+      val utcFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS Z")
+      val lastUpdated = utcFormat.parse(quote.lastUpdated.replace("Z", " UTC")).getTime / 1000
+      val tickerInfo = TokenTickerInfo(id, name, symbol, websiteSlug, market, rank, circulatingSupply, totalSupply, maxSupply,
+        price, volume24h, marketCap, percentChange1h, percentChange24h, percentChange7d, lastUpdated)
+
+      tickerinfos :+ tickerInfo
+    }
+
+    SeqTpro(f)
+  }
+
+}
